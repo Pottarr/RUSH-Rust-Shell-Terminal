bdf5fb36020a491c39139d9718a073b880ded4aa
Krakenlord <chananyu1301@gmail.com>
2024-11-04 22:04:14 +0700

    added these 2 helper files for a cleaner look

---
+++ src/services/piping.rs
@@ -0,0 +1,65 @@
+use crate::Terminal;
+use std::io::Read;
+use std::{fs::File, io::Write};
+use std::path::Path;
+
+impl Terminal {
+    pub fn piping_helper(&mut self, final_output: &mut String, all_contents: &Vec<(String, String)>) {
+        let index_of_pipe = self.command.iter().position(|x| x == "|").unwrap();
+        match self.command[index_of_pipe..].len() {
+            1 => {
+                final_output.push('\n');
+                final_output.push_str("Error: pipe to nothing");
+                } 
+            2 => {
+                match self.command[index_of_pipe+1].clone().as_str() {
+                    "find" => {
+                        final_output.push('\n');
+                        final_output.push_str("Please enter a phrase to find");
+                    }
+                    _ => {
+                        final_output.push('\n');
+                        final_output.push_str(format!("Can't pipe with command: {}", self.command[index_of_pipe+1].clone()).as_str());
+                    }
+                }
+            }
+            3 => {
+                let phrase = String::from(self.command[index_of_pipe + 2].clone());
+                match self.command[index_of_pipe+1].clone().as_str() {
+                    "find" => {
+                        for file in all_contents {
+                            let mut found = false;
+                            for line in file.1.split('\n') {
+                                if line.contains(&phrase) {
+                                    found = true;
+                                    final_output.push('\n');
+                                    final_output.push_str(format!("{}: {}", file.0, line).as_str());
+                                }
+                            }
+                            if !found {
+                                final_output.push('\n');
+                                final_output.push_str(format!("Phrase \"{}\" not found in {}", &phrase, file.0).as_str());
+                            }
+                        }
+                    }
+                    _ => {
+                        final_output.push('\n');
+                        final_output.push_str(format!("Can't pipe with command: {}", self.command[index_of_pipe+1].clone()).as_str());
+                    }
+                }
+            }
+            _ => {
+                match self.command[index_of_pipe+1].clone().as_str() {
+                    "find" => {
+                        final_output.push('\n');
+                        final_output.push_str("Too many number of phrase arguments to find");
+                    }
+                    _ => {
+                        final_output.push('\n');
+                        final_output.push_str(format!("Can't pipe with command: {}", self.command[index_of_pipe+1].clone()).as_str());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/services/redirect.rs
@@ -0,0 +1,58 @@
+use crate::Terminal;
+use std::{fs::File, io::Write};
+use std::path::Path;
+use std::fs::OpenOptions;
+
+impl Terminal {
+    pub fn redirect_helper(&mut self, final_output: &mut String, contents: String) {
+        let index_of_redirect = self.command.iter().position(|x| x == ">" || x == ">>").unwrap();
+        match self.command[index_of_redirect..].len() {
+            1 => {
+                final_output.push('\n');
+                final_output.push_str("No filename specified.");
+                self.output.push(final_output.to_string());
+            }
+            2 => {
+                let stupid_path = self.command[index_of_redirect+1].clone();
+                let stupid_path_2 = format!("{}/{}", self.current_path.to_str().unwrap(), self.command[index_of_redirect+1].clone());
+                let mut filepath= Path::new(stupid_path.as_str());
+                let mut file_open = & mut OpenOptions::new();
+                let mut file: File;
+                if filepath.has_root() {
+                    if filepath.exists() {
+                        file_open = file_open.write(true);
+                    } else {
+                        file_open = file_open.write(true).create_new(true);
+                    }
+                } else {
+                    filepath = Path::new(stupid_path_2.as_str());
+                    if filepath.exists() {
+                        file_open = file_open.write(true);
+                    } else {
+                        file_open = file_open.write(true).create_new(true);
+                    }
+                }
+                match self.command[index_of_redirect].clone().as_str() {
+                    ">" => {
+                        file = file_open.open(filepath).unwrap();
+                        if let Err(e) = writeln!(file, "{contents}") {
+                            eprintln!("Couldn't write to file: {e}")
+                        }
+                    }
+                    ">>" => {
+                        file = file_open.append(true).open(filepath).unwrap();
+                        if let Err(e) = writeln!(file, "{contents}") {
+                            eprintln!("Couldn't write to file: {e}")
+                        }
+                    }
+                    _ => unreachable!()
+                }
+            }
+            _ => {
+                final_output.push('\n');
+                final_output.push_str("Can only redirect to 1 file at a time.");
+                self.output.push(final_output.to_string());
+            }
+        }
+    }
+}
