08b9b883c4e0c6d5972721388f4011af73e90c95
Krakenlord <chananyu1301@gmail.com>
2024-11-05 15:31:42 +0700

    log now supports redirect

--- src/commands/log.rs
+++ src/commands/log.rs
@@ -5,20 +5,27 @@ impl Terminal {
         if self.command.len() == 1 {
             final_output.push_str("\nPlease specify the option being logged");
         } else {
-            match self.command[1..].join(" ").as_str() {
+            let mut contents = String::new();
+            match self.command[1].as_str() {
                 "-h" => {
-                    final_output.push_str("\n");
-                    final_output.push_str(format!("Command count in history: {}", self.history.len()).as_str());
+                    contents.push_str("\n");
+                    contents.push_str(format!("Command count in history: {}", self.history.len()).as_str());
                     for command in &self.history {
-                        final_output.push_str("\n");
-                        final_output.push_str(&command);
+                        contents.push_str("\n");
+                        contents.push_str(&command);
+                    }
+                    if self.command.contains(&">".to_string()) || self.command.contains(&">>".to_string()) {
+                        self.redirect_helper(final_output, contents);
+                    } else {
+                        final_output.push_str(&contents);
+                        self.output.push(final_output.to_string());
                     }
                 }
                 _ => {
                     final_output.push_str("\nInvalid arguments");
+                    self.output.push(final_output.to_string());
                 }
             }
         }
-        self.output.push(final_output.to_string());
     }
 }
